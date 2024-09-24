49015552c8e2ffa88112ef57d3d8f5b9f735ffe5
Balachandar-Sivkumar <balachandarsdckap@gmail.com>
2024-09-24 16:47:12 +0530

    Modified

--- index.js
+++ index.js
@@ -9,22 +9,24 @@ invisible.addEventListener("click",closer)
 function opener(){
 
          document.getElementsByClassName("header-ul")[0].style.display = "flex"
-          document.getElementById("searching").style.display = "none"
+          document.getElementById("searching").style.visibility = "hidden"
 }
 
 function closer(){
     document.getElementsByClassName("header-ul")[0].style.display = "none"
-    document.getElementById("searching").style.display = "block"
+    document.getElementById("searching").style.visibility = "visible"
    
 }
 
 window.addEventListener("resize",()=>{
     if(window.screen.width >1150){
          document.getElementsByClassName("header-ul")[0].style.display = ""
+         document.getElementById("searching").style.visibility = "hidden"
     }
 
     else{
          document.getElementsByClassName("header-ul")[0].style.display = "none"
+         document.getElementById("searching").style.visibility = "visible"
     }
 })
 
